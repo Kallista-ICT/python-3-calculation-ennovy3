a666e6fde46c453210a18ccc00c2be8b9ff924e4
ennovy3 <petrinnayvonne.sumolang@gmail.com>
2025-02-12 01:23:19 +0000

    Submit

---
+++ python/python_practice.py
@@ -0,0 +1,9 @@
+# Comment here
+distance = float(input("Enter distance in (metres): "))
+time = float(input("Enter distance in (seconds): "))
+
+# Comment here
+velocity = distance/time
+
+# Comment here
+print(f"The velocity is {velocity: .2f} m/s.")
